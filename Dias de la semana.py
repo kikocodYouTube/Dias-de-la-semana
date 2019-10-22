ff555824aa6cc326643450f4c606c52cd62e0376
Francisco Tejero <55576376+kikocodYouTube@users.noreply.github.com>
2019-10-22 11:52:48 +0200

    Add files via upload

---
+++ Dias de la semana.py
@@ -0,0 +1,20 @@
+def dias ():
+    a=input("Numero del dia= ")
+
+    if a is 1:
+        print ("Lunes")
+    elif a is 2:
+        print ("Martes")
+    elif a is 3:
+        print ("Miercoles")
+    elif a is 4:
+        print ("Jueves")
+    elif a is 5:
+        print ("Viernes")
+    elif a is 6:
+        print ("Sabado")
+    elif a is 7:
+        print ("Domingo")
+    elif a >= 7:
+        print ("No existe")
+dias()
